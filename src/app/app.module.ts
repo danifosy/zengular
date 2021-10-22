5a22342d79b026da4befd3c336aa08d9721a0e06
danifosy <mail@daniha.de>
2021-10-22 16:18:29 +0200

    Write basic app.module.ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+// allows to pass metadata to describe how to compile + inject code at runtime
+// only one root module per app, thats why we import other modules in array
+@NgModule({
+  imports: [BrowserModule],
+})
+// modules organize app and extend them with other modules
+export class AppModule {}
