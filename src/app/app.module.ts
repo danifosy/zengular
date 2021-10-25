7801d22b48daa564e2e3be92b3a7b6cc527bec59
danifosy <mail@daniha.de>
2021-10-25 11:48:28 +0200

    Add bootstrap property

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  bootstrap: [AppComponent],
   declarations: [AppComponent],
   imports: [BrowserModule],
 })
