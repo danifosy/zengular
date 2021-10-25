42cd78d0b44314887b8129160c5f857d79770622
danifosy <mail@daniha.de>
2021-10-25 11:49:05 +0200

    Add selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,13 @@
+// decorator for anuglar to know this is a component
 import { Component } from '@angular/core';
+// app component = root component
 
+// {} = configuration object
 @Component({
+  // provides reference for sth that can be selected in body and injects component into it
+  // see app-root from index.html
+  selector: 'app-root',
+  // syntax used to render app
   template: '<p>Hello World</p>',
 })
 export class AppComponent {}
