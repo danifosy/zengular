4b4683908a61f72a8a81081b7c831f3df66bb8a2
danifosy <mail@daniha.de>
2021-10-25 11:26:53 +0200

    Import AppComponent into app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 // allows to pass metadata to describe how to compile + inject code at runtime
 // only one root module per app, thats why we import other modules in array
+import { AppComponent } from './app.component';
+
 @NgModule({
+  declarations: [AppComponent],
   imports: [BrowserModule],
 })
 // modules organize app and extend them with other modules
