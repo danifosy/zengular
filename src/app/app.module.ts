0ef288b1b949dd79dfde2b780bd7f6abe4be1799
danifosy <mail@daniha.de>
2021-11-16 13:17:11 +0100

    Create question component and refactor question

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
+import { QuestionComponent } from './question/question.component';
 
 // allows to pass metadata to describe how to compile + inject code at runtime
 // only one root module per app, thats why we import other modules in array
@@ -11,7 +12,7 @@ import { AppComponent } from './app.component';
   // property that allows to provide array of components that will be initialized when app starts
   bootstrap: [AppComponent],
   // declare components used in app
-  declarations: [AppComponent],
+  declarations: [AppComponent, QuestionComponent],
   imports: [BrowserModule, NgbModule, FontAwesomeModule],
 })
 // modules organize app and extend them with other modules
